5c5651ad742b4678506af375473e3479d36a0886
Phahose <ekwomnick@gmail.com>
2024-08-07 19:56:29 -0600

    The messages page done

---
+++ Oncourt/Pages/Messages.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnCourt.Pages
+{
+    public class MessagesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Oncourt/wwwroot/js/script.js
@@ -0,0 +1,17 @@
+﻿const swiper = new Swiper('.swiper', {
+    effect: 'cube',
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
